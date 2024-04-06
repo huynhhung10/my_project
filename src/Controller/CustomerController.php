47d6fe86a46fb4dfe46de18e36626928ffb61d73
Tran Minh Thuc <tranminhthuc79@gmail.com>
2024-04-06 17:22:01 +0700

    customer

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -6,84 +6,97 @@ use App\Entity\Customers;
 use App\Form\CustomersFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class CustomerController extends AbstractController
-{
-      /**
+/**
+ * Customer Controller.
+ */
+class CustomerController extends AbstractController {
+  /**
    * Entity manager.
    *
-   * @var \Doctrine\ORM\EntityManagerInterface
+   * @var \Doctrine\ORM\EntityManagerInterface The entity manager instance It manages the database interactionsManages database interactions
    */
   private $em;
 
   /**
    * Constructor.
    *
-   * @param \Doctrine\ORM\EntityManagerInterface $entityManager
-   *   The entity manager instance.
+   * @param \Doctrine\ORM\EntityManagerInterface $em
+   *   The entity manager instance to manage database interactions.
    */
-  public function __construct(EntityManagerInterface $em)
-  {
+  public function __construct(EntityManagerInterface $em) {
     $this->em = $em;
   }
 
-    #[Route('/customers', name: 'customers')]
-    public function index(): Response
-    {
-        $customers = $this->em->getRepository(Customers::class)->findAll();
-        return $this->render('customer/index.html.twig', [
-            'customers' => $customers,
-        ]);
-    }
-    
-    #[Route('/admin/create-customer', name: 'create-customer')]
-    public function createCustomer(Request $request) {
-        $customer = new Customers();
-        $form = $this->createForm(CustomersFormType::class, $customer);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $this->em->persist($customer);
-            $this->em->flush();
+  /**
+  * Show customers.
+  */
+  #[Route('/customers', name: 'customers')]
+  public function index(): Response {
+    $customers = $this->em->getRepository(Customers::class)->findAll();
+    return $this->render('customer/index.html.twig', [
+      'customers' => $customers,
+    ]);
+  }
 
-            $this->addFlash('insert', 'true');
-            return $this->redirectToRoute('customers');
-        }
-        return $this->render('customer/customer.html.twig',[
-            'form' => $form->createView(),
-        ]);
-    }
+  /**
+  * Create customer.
+  */
+  #[Route('/admin/create-customer', name: 'create-customer')]
+  public function createCustomer(Request $request) {
+    $customer = new Customers();
+    $form = $this->createForm(CustomersFormType::class, $customer);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->em->persist($customer);
+      $this->em->flush();
 
-    #[Route('/admin/edit-customer/{id}', name: 'edit-customer')]
-    public function editCustomer(Request $request, $id) {
-        
-        $customer = $this->em->getRepository(Customers::class)->find($id);
-        $form = $this->createForm(CustomersFormType::class, $customer);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()) {
-            $this->em->persist($customer);
-            $this->em->flush();
-
-            $this->addFlash('update', 'true');
-            return $this->redirectToRoute('customers');
-        }
-        return $this->render('customer/customer.html.twig',[
-            'form' => $form->createView(),
-        ]);
+      $this->addFlash('insert', 'true');
+      return $this->redirectToRoute('customers');
     }
-    
-    #[Route('/admin/delete-customer/{id}', name: 'delete-customer')]
-    public function deleteCustomer(Request $request, $id) {
-        $customer = $this->em->getRepository(Customers::class)->find($id);
-        if($customer) {
-            $this->em->remove($customer);
-            $this->em->flush();
+    return $this->render('customer/customer.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
 
-            $this->addFlash('delete', 'true');
-            return $this->redirectToRoute('customers');
-        }
-        return new Response('Invalid customer data', Response::HTTP_BAD_REQUEST);
+  /**
+  * Edit customer.
+  */
+  #[Route('/admin/edit-customer/{id}', name: 'edit-customer')]
+  public function editCustomer(Request $request, $id) {
+
+    $customer = $this->em->getRepository(Customers::class)->find($id);
+    $form = $this->createForm(CustomersFormType::class, $customer);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->em->persist($customer);
+      $this->em->flush();
+
+      $this->addFlash('update', 'true');
+      return $this->redirectToRoute('customers');
     }
+    return $this->render('customer/customer.html.twig', [
+      'form' => $form->createView(),
+    ]);
+  }
+
+  /**
+  * Delete a customer.
+  */
+  #[Route('/admin/delete-customer/{id}', name: 'delete-customer')]
+  public function deleteCustomer(Request $request, $id) {
+    $customer = $this->em->getRepository(Customers::class)->find($id);
+    if ($customer) {
+      $this->em->remove($customer);
+      $this->em->flush();
+
+      $this->addFlash('delete', 'true');
+      return $this->redirectToRoute('customers');
+    }
+    return new Response('Invalid customer data', Response::HTTP_BAD_REQUEST);
+  }
+
 }
